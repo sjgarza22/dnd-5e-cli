68eb5713584dfbf2df224474bea6c1fcbc76ab67
sjgarza22 <sjgarza22@gmail.com>
2020-05-09 18:25:27 -0500

    Created methods and class variables

--- lib/handbook/player_class.rb
+++ lib/handbook/player_class.rb
@@ -1,13 +1,35 @@
 class PlayerClass
-
+    @attr_accessor :hit_die
+    @attr_reader :name
+    ENDPOINT = "classes"
     @@all = []
 
-    def initialize(name, hit_die, proficiency_choices, proficiency_num)
-
+    def initialize(name, hit_die, prof_choices, prof_choices_amt, proficiencies, saving_throws, equipment, class_levels, subclasses, spellcasting)
+        @name = name
         save
     end
 
     def self.save
         @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.endpoint
+        ENDPOINT
+    end
+
+    def find_by_name(name)
+        self.all.find {|spell| spell.name == name}
+    end
+
+    def find_or_create_by_name(name)
+        if !(find_by_name(name))
+            create(name)
+        else
+            find_by_name(name)
+        end
     end
 end
--- lib/handbook/spell.rb
+++ lib/handbook/spell.rb
@@ -1,3 +1,34 @@
 class Spell
+    attr_reader :name
+    ENDPOINT = "spells"
+    @@all = []
 
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.endpoint
+        ENDPOINT
+    end
+
+    def find_by_name(name)
+        self.all.find {|spell| spell.name == name}
+    end
+
+    def find_or_create_by_name(name)
+        if !(find_by_name(name))
+            create(name)
+        else
+            find_by_name(name)
+        end
+    end
 end

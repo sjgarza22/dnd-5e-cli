40857ad74af329035cfd1d97a2aa93bfa6ffecd8
sjgarza22 <sjgarza22@gmail.com>
2020-05-17 01:01:22 -0500

    Added all neccesary requirements

--- lib/environment.rb
+++ lib/environment.rb
@@ -7,4 +7,8 @@ require 'json' ## Parsing data from API in JSON
 require_relative "./handbook/cli.rb"
 require_relative "./handbook/api.rb"
 require_relative "./handbook/player_class.rb"
+require_relative "./handbook/race.rb"
+require_relative "./handbook/equipment.rb"
+require_relative "./handbook/spell.rb"
+require_relative "./handbook/monster.rb"
 

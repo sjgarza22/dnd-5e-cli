9f3d473e9229c66bb0664b7112e814ed7822d8e6
sjgarza22 <sjgarza22@gmail.com>
2020-05-25 01:32:03 -0500

    Add Find_by_Name & Find_or_Create_by_Name Methods

--- lib/handbook/concerns/findable.rb
+++ lib/handbook/concerns/findable.rb
@@ -1,3 +1,13 @@
 module Concerns::Findable
-    
+    def find_by_name(name)
+        all.find {|object| object.name == name}
+    end
+
+    def find_or_create_by_name(name, url)
+        if !(find_by_name(name))
+            create(name, url)
+        else
+            find_by_name(name)
+        end
+    end
 end

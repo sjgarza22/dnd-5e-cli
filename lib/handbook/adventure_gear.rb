5b8183a4dda9be1ff481fbefe63194bd0013e71a
sjgarza22 <sjgarza22@gmail.com>
2020-05-25 03:27:10 -0500

    Create AdventureGear class w/ 3 methods

---
+++ lib/handbook/adventure_gear.rb
@@ -0,0 +1,30 @@
+class AdventureGear < Equipment
+
+    def self.create(name, url, description = nil)
+        self.new(name, url)
+        @description = description
+    end
+
+    def fill_attributes(data)
+        @equipment_category = data["equipment_category"]
+        @gear_category = data["gear_category"]
+        @cost = "#{data["cost"]["quantity"]} #{data["cost"]["unit"]}"
+        @weight = data["weight"]
+        if data.key?("desc")
+            @description = data["desc"]
+        end
+
+    end
+
+    def print
+        puts "#{@name}\n\n"
+        puts "Equipment Category: #{@equipment_category}\n\n"
+        puts "Gear Category: #{@gear_category}\n\n"
+        puts "Cost: #{@cost}\n\n"
+        puts "Weight: #{@weight}\n\n"
+        if @description != nil
+            puts "Description: #{@description}\n\n"
+        end
+    end
+
+end

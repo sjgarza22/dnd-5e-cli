afabd2e7467b2fad7d12181148d2b5825eceaab2
sjgarza22 <sjgarza22@gmail.com>
2020-05-25 02:48:35 -0500

    Added load_attributes method and new constant

--- lib/handbook/api.rb
+++ lib/handbook/api.rb
@@ -1,10 +1,18 @@
 class Api
     BASE_URL = "http://www.dnd5eapi.co/api/"
+    DATA_BASE_URL = "http://www.dnd5eapi.co"
 
 
     # load method will use base url and provided endpoint to GET data
     def self.load(endpoint)
         response = Net::HTTP.get(URI("#{BASE_URL}#{endpoint}")) # this is how we'll grab our data
+        data = json_to_array(response)  # converts the JSON to a Ruby friendly array of hashes
+        # binding.pry
+        data
+    end
+
+    def self.load_attributes(endpoint)
+        response = Net::HTTP.get(URI("#{DATA_BASE_URL}#{endpoint}")) # this is how we'll grab our data
         data = json_to_array(response)  # converts the JSON to a Ruby friendly array of hashes
         # binding.pry
         data

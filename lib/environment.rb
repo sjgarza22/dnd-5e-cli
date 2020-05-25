230fec92f20a24e95d26c1ad2f230b720c656ce4
sjgarza22 <sjgarza22@gmail.com>
2020-05-25 15:31:37 -0500

    Removed old line 13, added line 7 & 13-17

--- lib/environment.rb
+++ lib/environment.rb
@@ -4,11 +4,17 @@ require 'pry'
 require 'httparty' ## Interacting with API
 require 'json' ## Parsing data from API in JSON
 
+require_relative "./handbook/concerns/findable.rb"
 require_relative "./handbook/cli.rb"
 require_relative "./handbook/api.rb"
 require_relative "./handbook/player_class.rb"
 require_relative "./handbook/race.rb"
 require_relative "./handbook/equipment.rb"
+require_relative "./handbook/weapon.rb"
+require_relative "./handbook/armor.rb"
+require_relative "./handbook/adventure_gear.rb"
+require_relative "./handbook/tool.rb"
+require_relative "./handbook/vehicle.rb"
 require_relative "./handbook/spell.rb"
-require_relative "./handbook/monster.rb"
+
 

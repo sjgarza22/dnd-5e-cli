8d22125ff495abcfefaeb0e7112092fb950870ab
sjgarza22 <sjgarza22@gmail.com>
2020-05-25 16:50:51 -0500

    Add extend, add Create, fill_attributes & print

--- lib/handbook/spell.rb
+++ lib/handbook/spell.rb
@@ -1,10 +1,13 @@
 class Spell
-    attr_reader :name
+    extend Findable
+
+    attr_reader :name, :url
     ENDPOINT = "spells"
     @@all = []
 
-    def initialize(name)
+    def initialize(name, url)
         @name = name
+        @url = url
         save
     end
 
@@ -20,15 +23,52 @@ class Spell
         ENDPOINT
     end
 
-    def self.find_by_name(name)
-        self.all.find {|spell| spell.name == name}
+    def self.create(name, url, higher_level = nil)
+        @higher_level = higher_level
+        @components = []
+        @classes = []
+        @subclasses = []
+        self.new(name, url)
     end
 
-    def self.find_or_create_by_name(name)
-        if !(find_by_name(name))
-            Spell.new(name)
-        else
-            find_by_name(name)
+    def fill_attributes(data)
+        @description = data["desc"]
+        if data.key?("higher_level")
+            @higher_level = data["higher_level"]
         end
+        @range = data["range"]
+        data["components"].each {|component| @components << component}
+        @material = data["material"]
+        @ritual = data["ritual"]
+        @duration = data["duration"]
+        @concentration = data["concentration"]
+        @casting_time = data["casting_time"]
+        @level = data["level"]
+        @school = data["school"]["name"]
+        data["classes"].each {|spell_class| @classes << spell_class["name"]}
+        data["subclasses"].each {|subclass| @subclasses << subclass["name"]}
+    end
+
+    def print
+        puts "#{@name}\n\n"
+        puts "Description: #{@description}\n\n"
+        if @higher_level != nil
+            puts "Higher Level: #{@higher_level}\n\n"
+        end
+        puts "Range: #{@range}\n\n"
+        puts "Components:"
+        @components.each {|component| puts component}
+        puts "\nMaterial: #{@material}\n\n"
+        puts "Ritual: #{@ritual}\n\n"
+        puts "Duration: #{@duration}\n\n"
+        puts "Concentration: #{@concentration}\n\n"
+        puts "Casting Time: #{@casting_time}\n\n"
+        puts "Level: #{@level}\n\n"
+        puts "School: #{@school}\n\n"
+        puts "Classes:"
+        @classes.each {|spell_class| puts spell_class}
+        puts "\nSubclasses:"
+        @subclasses.each {|subclass| puts subclass}
+        puts " "
     end
 end

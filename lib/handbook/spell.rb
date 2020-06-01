981607f415158ca856c15030190066ed0705dcf7
sjgarza22 <sjgarza22@gmail.com>
2020-06-01 15:38:14 -0500

    Fixed init, fill_attributes & print

--- lib/handbook/spell.rb
+++ lib/handbook/spell.rb
@@ -23,11 +23,9 @@ class Spell
         ENDPOINT
     end
 
-    def self.create(name, url, higher_level = nil)
+    def self.create(name, url, higher_level = nil, material = nil)
+        @material = material
         @higher_level = higher_level
-        @components = []
-        @classes = []
-        @subclasses = []
         self.new(name, url)
     end
 
@@ -37,14 +35,19 @@ class Spell
             @higher_level = data["higher_level"]
         end
         @range = data["range"]
+        @components = []
         data["components"].each {|component| @components << component}
-        @material = data["material"]
+        if data.key?("material")
+            @material = data["material"]
+        end
         @ritual = data["ritual"]
         @duration = data["duration"]
         @concentration = data["concentration"]
         @casting_time = data["casting_time"]
         @level = data["level"]
         @school = data["school"]["name"]
+        @classes = []
+        @subclasses = []
         data["classes"].each {|spell_class| @classes << spell_class["name"]}
         data["subclasses"].each {|subclass| @subclasses << subclass["name"]}
     end
@@ -58,7 +61,9 @@ class Spell
         puts "Range: #{@range}\n\n"
         puts "Components:"
         @components.each {|component| puts component}
-        puts "\nMaterial: #{@material}\n\n"
+        if @material != nil
+            puts "\nMaterial: #{@material}\n\n"
+        end
         puts "Ritual: #{@ritual}\n\n"
         puts "Duration: #{@duration}\n\n"
         puts "Concentration: #{@concentration}\n\n"
